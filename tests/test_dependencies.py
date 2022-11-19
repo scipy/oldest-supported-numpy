528099b782e53ab25e56b84c989ab38f8be824a5
Ralf Gommers <ralf.gommers@gmail.com>
2022-11-19 17:34:21 +0100

    TST: improve test for OS400

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -43,14 +43,11 @@ def test_has_at_most_one_pinned_dependency(
     platform_python_implementation,
 ):
     # These are known to be platforms that are not valid / possible at this time.
-    if platform_system == "AIX":
+    if platform_system in ("AIX", "OS400"):
         if platform_machine in ["aarch64", "loongarch64"]:
-            pytest.skip("AIX and aarch64 are mutually exclusive.")
+            pytest.skip(f"{platform_system} and {platform_machine} are mutually exclusive.")
         if platform_python_implementation == "PyPy":
-            pytest.skip("AIX and PyPy are mutually exclusive.")
-
-    if platform_system == "OS400":
-        pytest.skip("Skip this test for OS400.")
+            pytest.skip(f"{platform_system} and PyPy are mutually exclusive.")
 
     if platform_python_implementation == "PyPy" and (platform_machine not in ["x86_64", "aarch64"]):
         pytest.skip(f"PyPy is not supported on {platform_machine}.")
